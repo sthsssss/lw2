b11cd7887e985450b8c191ad608d71f31b80e1a6
GomSon-E <gomsonixx@gmail.com>
2022-09-27 03:20:37 +0900

    Add intent

--- app/src/main/java/org/techtown/lotto_cardflip/MainActivity.java
+++ app/src/main/java/org/techtown/lotto_cardflip/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button analysisBtn = findViewById(R.id.analysisBtn);
+        analysisBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numIntent = new Intent(getApplicationContext(), NumAnalysis.class);
+                numIntent.putExtra("numData", Arrays.copyOfRange(nums, 0, 6));
+            }
+        });
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
